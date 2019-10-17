4bb0c51f8e37472003c188884ef5c0f06110a785
BuckeyeFan084 <56597617+BuckeyeFan084@users.noreply.github.com>
2019-10-17 18:34:28 +0000

    Done with FEW100

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
 import { getRandomInt } from './util';
 
 let squares: NodeListOf<HTMLDivElement>;
+let message: HTMLElement;
 export function runApp() {
     // 1. Find alll the squares.
-    const squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
-    const secret = getSecretNumber();
+    message = document.getElementById('message') as HTMLElement;
 
+    squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+    const secret = getSecretNumber();
     squares.forEach((sq, index) => {
         if (index === secret) {
             sq.dataset.secret = 'true';
         }
         sq.addEventListener('click', handleClick);
+
     });
     // 2. pick one as the secret square and "mark it".
     // 3. make it so that when the player clicks the square...
 
 }
+
+let tries = 0;
 
 function handleClick(e: any) {
     const clickedSquare = this as HTMLDivElement;
@@ -25,11 +30,23 @@ function handleClick(e: any) {
         message.innerText = 'Whoa! Awesome! You found it!';
         squares.forEach(s => {
             if (s !== clickedSquare) {
-                s.classList.add('loser');
+                s.classList.add('winner');
+                s.removeEventListener('click', handleClick);
             }
         });
     } else {
         clickedSquare.classList.add('loser');
+        tries++;
+        if (tries === 5) {
+            squares.forEach(sq => {
+                if (sq.dataset.secret === 'true') {
+                    sq.classList.add('winner');
+                } else {
+                    sq.classList.add('loser');
+                }
+                message.innerText = 'Wow! You Suck! I wouldn\'t play the lottery if I were you!';
+            });
+        }
     }
 }
 
